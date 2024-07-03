6f3f91c80b63767114abb3463b2e0f3ad9f14d7d
MixJade <111669273+MixJade@users.noreply.github.com>
2024-07-03 20:04:40 +0800

    图片使用本地服务器

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express'); // 引用express库
+const app = express(); // 创建express应用
+const path = require('path');
+
+// `__dirname`变量是当前脚本所在的目录
+// 访问`http://127.0.0.1:9527/pic`，就等于访问`public`文件夹
+app.use('/pic', express.static(path.join(__dirname, 'public')));
+
+app.listen(9527, function () {
+    // 服务器正在监听9527端口
+    console.log('Server is running at http://127.0.0.1:9527/pic');
+});
