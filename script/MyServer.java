a10012d772bf80191aeef6099b5cfb2f09549ed5
MixJade <111669273+MixJade@users.noreply.github.com>
2024-10-17 10:14:43 +0800

    Update MyServer.java

--- script/MyServer.java
+++ script/MyServer.java
@@ -13,11 +13,11 @@ import java.util.concurrent.Executors;
  *
  * @since 2024-10-01 20:09:39
  */
-public class MyServer {
+public class MyServer implements HttpHandler {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(9527), 0);
-        server.createContext("/public", new MyHandler());
+        server.createContext("/public", new MyServer());
 
         server.setExecutor(Executors.newFixedThreadPool(1));
         server.start();
@@ -26,34 +26,37 @@ public class MyServer {
         System.out.println("测试访问: http://localhost:9527/public/showhtml/svgdesktop.html?wp003.jpg");
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange httpExchange) {
-            try {
-                // 从java的执行路径开始算
-                String path = ".." + httpExchange.getRequestURI().getPath();
-                System.out.println("访问" + path);
-                File file = new File(path);
-                if (file.isFile()) {
-                    byte[] bytes = Files.readAllBytes(file.toPath());
-                    //为.svg后缀的请求设置响应头
-                    if (httpExchange.getRequestURI().toString().endsWith(".svg")) {
-                        httpExchange.getResponseHeaders().add("Content-Type", "image/svg+xml");
-                    }
-                    httpExchange.sendResponseHeaders(200, bytes.length);
-                    OutputStream os = httpExchange.getResponseBody();
-                    os.write(bytes);
-                    os.close();
-                } else {
-                    String response = "File not found.";
-                    httpExchange.sendResponseHeaders(200, response.length());
-                    OutputStream os = httpExchange.getResponseBody();
-                    os.write(response.getBytes());
-                    os.close();
+    @Override
+    public void handle(HttpExchange httpExchange) {
+        try {
+            // 从java的执行路径开始算
+            String path = ".." + httpExchange.getRequestURI().getPath();
+            System.out.println("访问" + path);
+            File file = new File(path);
+            if (file.isFile()) {
+                byte[] bytes = Files.readAllBytes(file.toPath());
+                // 为.svg后缀的请求设置响应头
+                String httpFileName = httpExchange.getRequestURI().toString();
+                if (httpFileName.endsWith(".svg")) {
+                    httpExchange.getResponseHeaders().add("Content-Type", "image/svg+xml");
+                } else if (httpFileName.endsWith(".js")) {
+                    httpExchange.getResponseHeaders().add("Content-Type", "application/javascript; charset=utf-8");
+                } else if (httpFileName.endsWith(".css")) {
+                    httpExchange.getResponseHeaders().add("Content-Type", "text/css; charset=utf-8");
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+                httpExchange.sendResponseHeaders(200, bytes.length);
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(bytes);
+                os.close();
+            } else {
+                String response = "File not found.";
+                httpExchange.sendResponseHeaders(200, response.length());
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
