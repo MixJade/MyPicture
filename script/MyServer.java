6eb318de4b6ac17867950fef23f45bdfbab17cd9
MixJade <111669273+MixJade@users.noreply.github.com>
2025-06-09 18:06:48 +0800

    java服务器更新

--- script/MyServer.java
+++ script/MyServer.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 /**
@@ -14,23 +16,36 @@ import java.util.concurrent.Executors;
  * @since 2024-10-01 20:09:39
  */
 public class MyServer implements HttpHandler {
+    private static final String REQ_CONTEXT = "/public";
+    private static final String REQ_FOLDER = "..";
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(9527), 0);
-        server.createContext("/public", new MyServer());
+        server.createContext(REQ_CONTEXT, new MyServer());
 
         server.setExecutor(Executors.newFixedThreadPool(1));
         server.start();
         System.out.println("服务器已启动");
-        System.out.println("测试访问: http://localhost:9527/public/wallpaper/wp003.jpg");
-        System.out.println("测试访问: http://localhost:9527/public/showhtml/svgdesktop.html?wp003.jpg");
+        System.out.println("测试访问:http://localhost:9527" + REQ_CONTEXT + "/wallpaper/wp003.jpg");
+        System.out.println("测试访问:http://localhost:9527" + REQ_CONTEXT + "/showhtml/svgdesktop.html?wp003.jpg");
+        System.out.println("测试访问:http://localhost:9527" + REQ_CONTEXT + "/list");
     }
 
     @Override
     public void handle(HttpExchange httpExchange) {
         try {
+            // 另一个执行器(此处额外逻辑)
+            if ((REQ_CONTEXT + "/list").equals(httpExchange.getRequestURI().getPath())) {
+                String response = getImgFileList();
+                httpExchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+                return;
+            }
             // 从java的执行路径开始算
-            String path = ".." + httpExchange.getRequestURI().getPath();
+            String path = REQ_FOLDER + httpExchange.getRequestURI().getPath();
             System.out.println("访问" + path);
             File file = new File(path);
             if (file.isFile()) {
@@ -58,5 +73,24 @@ public class MyServer implements HttpHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 输出目标文件夹下的图片列表
+     */
+    private String getImgFileList() {
+        // 指定要读取的文件夹路径
+        File folder = new File(REQ_FOLDER + REQ_CONTEXT + "/wallpaper");
+        List<String> fileNames = new ArrayList<>();
+        if (folder.exists() && folder.isDirectory()) {
+            // 输出文件或文件夹名称
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.getName().endsWith(".jpg")) fileNames.add(file.getName());
+                }
+            } else System.out.println("该文件夹为空。");
+        } else System.out.println("指定的路径不存在或者不是一个有效的文件夹。");
+        return String.join(",", fileNames);
     }
 }
